7d46d43d42cd74328e39e467e7b387e96eeb56b4
sonhoang2503 <skylarnguyen25@gmail.com>
2022-01-14 14:27:17 +0700

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6f344ae7-dde5-4ae8-9b92-daef8ccc3d90';
+export const MODAL_CLOSE_SEC = 2.5;
